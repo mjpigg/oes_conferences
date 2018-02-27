8b5c1b455c2c0688fe45cbc426fa3e6b12c8c9c1
Matthew Pigg <mjpigg@gmail.com>
2018-02-27 13:09:39 -0800

    latest function changes to evolve and scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,35 +1,12 @@
-import datetime,sqlite3,random
+from evolve import *
 
-def create_empty_schedule():
-    delta=datetime.timedelta(minutes=30)
-    start=datetime.datetime(2018,2,28,7,30)
-    n=1
-    c=start
-    a=1
-    sch={}
-    breaks=['9:30','12:0','12:30','14:30']
-    while 1:
-        the_time = "{}:{}".format(c.hour, c.minute)
-        #print(the_time)
-        if c>datetime.datetime(2018,3,2,15):
-            break
-        if the_time in breaks and (c.weekday!=4 and the_time!='14:30'):
-           # print('BREAK')
-            c=c+delta
-            continue
-        if c>datetime.datetime(c.year,c.month,c.day,16,30):
-            c=start+datetime.timedelta(hours=24*n)
-            n+=1
-            continue
-        #print("{}: {}".format(a,c))
-        #print(str(c))
-        sch[str(c)]=[]
-        a+=1
-        c = c + delta
-    return(sch)
+pop = create_population(100)
 
+top = 0
+for x, y in enumerate(pop):
+    if y[0] > top:
+        top = y[0]
+        best = x
+    #print("{}".format(y[0]),end = '\t')
 
-
-
-#print(get_prefs())
-
+print("Best Fitness: {}".format(pop[best][0]))
