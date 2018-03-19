f4f1d4fd800417e6a2e96004006a86bb68775b62
Matthew Pigg <mjpigg@gmail.com>
2018-03-19 16:43:11 -0700

    added double booked and advisee conflicts

--- hello.py
+++ hello.py
@@ -35,11 +35,17 @@ def show_confs(teacher = 'all'):
     cur.execute(sql)
     rows = cur.fetchall()
     confs = []
+    adv_times={}
+    bookings = {}
     for i in rows:
+        adv_times[i[2]]= adv_times.get(i[2],[])+[i[6]]
+        bookings[i[2]] = bookings.get(i[2], []) + [i[7],i[8],i[9],i[10],i[11],i[12]]
         confs.append(i)
     cur.close()
     con.close()
-    return render_template("full_conf.html", conf=confs, the_total=len(confs),teacher=teacher.upper())
+    # bookings = {'2018-03-01 08:00:00':{'Phelps':'**'}}
+    return render_template("full_conf.html", conf=confs, the_total=len(confs),teacher=teacher.upper(),adv_times=adv_times, bookings=bookings)
+
 
 
 @app.route('/conf/<teacher>')
